58f0bc35911a4d0f2af63450b59b87b6a1b13788
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-11 12:29:25 -0500

    Impl

---
+++ src/lookup.lisp
@@ -0,0 +1,22 @@
+(defun lookup (k m) 
+    (if (listp m)
+        (if (listp (first m))
+            (if (eq k (first (first m)))
+                (second (first m))
+                (if (endp (rest m))
+                    'nil
+                    (lookup k (rest m))
+                )
+            )
+            'nil
+        )
+        'nil
+    )
+)
+
+; (lookup 'a '((a b) (c d)))       ; => b
+; (lookup 'e '((a b) (c d)))       ; => nil
+; (lookup 'a '((b a) (c d)))       ; => nil
+; (lookup 'e '((a b) (c d) (e f))) ; => f
+; (lookup 'c '((a b) (c (d e f)))) ; => (d e f)
+
